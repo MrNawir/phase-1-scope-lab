a011a314b3af7fab623b171147de78dcbba3007c
MidasCodex <nawir.ibrahim@strathmore.edu>
2025-09-10 10:10:48 +0300

    Finished Work

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// 1. Declare customerName in global scope using var
+var customerName = 'bob';
+
+// 2. Function to uppercase customerName
+function upperCaseCustomerName() {
+	customerName = customerName.toUpperCase();
+}
+
+// 3. Function to set bestCustomer in global scope
+function setBestCustomer() {
+	bestCustomer = 'not bob'; // Implicit global
+}
+
+// 4. Function to overwrite bestCustomer
+function overwriteBestCustomer(newCustomer) {
+	bestCustomer = newCustomer;
+}
+
+// 5. Declare leastFavoriteCustomer as a global constant
+const leastFavoriteCustomer = 'someone';
+
+// 6. Function to attempt to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+	leastFavoriteCustomer = 'someone else'; // Will throw error
+}
